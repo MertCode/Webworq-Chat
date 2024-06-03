7d735772989d8835251b168ce075623bbc458af5
Mert Sozen <mertcode0@gmail.com>
2024-06-03 14:50:14 +0200

    Refactor Group, Message, and User models

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -10,4 +10,23 @@ class Group extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'description', 'owner_id', 'last_message_id'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'group_users');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    
+
+
 }
