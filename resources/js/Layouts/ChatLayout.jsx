c10aa5dbd9ea2e8429ecf5a2db8532cd4e878535
MertCode <mertcode0@gmail.com>
2024-06-05 05:08:25 +0200

    configured persistent layout with react

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,25 +1,19 @@
 import { usePage } from "@inertiajs/react";
-import AuthenticatedLayout from "@Layouts/AuthenticatedLayout";
 
-
-
-const ChatLayout = ({ Children }) => {
-
+const ChatLayout = ({ children }) => {
     const page = usePage();
     const conversations = page.props.conversations;
     const selectedConversation = page.props.selectedConversation;
 
-
     console.log("conversations", conversations);
     console.log("selectedConversations", selectedConversation);
+
     return (
-        <AuthenticatedLayout>
-            Chat Layoutzddzd
-            <div> {Children}</div>
-        </AuthenticatedLayout>
-
+        <>
+            ChatLayout
+            <div>{children}</div>
+        </>
     );
-
-}
+};
 
 export default ChatLayout;
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,21 +1,16 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import ChatLayout from "@/Layouts/ChatLayout";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 
-export default function Home({ auth }) {
+function Home({ auth }) {
+    return <>messages</>;
+}
+
+Home.layout = (page) => {
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
-        >
-            <Head title="Dashboard" />
-
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
-                    </div>
-                </div>
-            </div>
+        <AuthenticatedLayout user={page.props.auth.user}>
+            <ChatLayout children={page}></ChatLayout>
         </AuthenticatedLayout>
     );
-}
+};
+
+export default Home;
