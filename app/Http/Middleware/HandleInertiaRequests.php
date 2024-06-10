1d1a6237e3a6ba68074a948cffa809096d6c1d5b
MertCode <mertcode0@gmail.com>
2024-06-11 01:26:40 +0200

    Add authenticated user's conversations to sidebar

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -4,6 +4,11 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Inertia\Middleware;
+use App\Models\Conversation;
+use Illuminate\Support\Facades\Auth;
+
+
+
 
 class HandleInertiaRequests extends Middleware
 {
@@ -34,6 +39,7 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
             ],
+            'conversations' => Auth::id() ? Conversation::getConversationsForSidebar(Auth::user()) : [],
         ];
     }
 }
