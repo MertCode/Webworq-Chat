9f0a6a32780b4ec39b18028be46b3512a8a1faac
MertCode <mertcode0@gmail.com>
2024-06-13 01:34:05 +0200

    Add MessageController with methods for loading user messages

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -4,8 +4,46 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Group;
+use App\Models\Message;
+use App\Http\Requests\StoreMessageRequest;
+
 
 class MessageController extends Controller
 {
-    //
+    // this loads messages by users
+    public function byUser(User $user)
+    {
+        $messages = Message::where('sender_id', auth()->id())
+            ->where('receiver_id', $user->id)
+            ->orWhere('sender_id', $user->id)
+            ->where('receiver_id', auth()->id())
+            ->latest()
+            ->paginate(10);
+
+        return inertia('Home', [
+            'selectedConversation' => $user->toConversationArray(),
+        ]);
+    }
+
+    // this loads messages by groups
+    public function byGroup(Group $group)
+    {
+    }
+
+    // this loads older messages - limited to provided method
+    public function loadOlder(Message $message)
+    {
+    }
+
+    // this stores a new message
+    public function store(StoreMessageRequest $request)
+    {
+    }
+
+    // this deletes a message
+    public function destroy(Message $message)
+    {
+    }
 }
