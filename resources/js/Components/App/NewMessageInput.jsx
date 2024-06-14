2b4dd98cae15a011fa9af4b168eeebca26f79ab5
MertCode <mertcode0@gmail.com>
2024-06-14 04:45:19 +0200

    typo in class

--- resources/js/Components/App/NewMessageInput.jsx
+++ resources/js/Components/App/NewMessageInput.jsx
@@ -35,7 +35,7 @@ const NewMessageInput = ({ value, onChange, onSend }) => {
             placeholder="Type a message"
             onKeyDown={onInputKeyDown}
             onChange={(ev) => onChangeEvent(ev)}
-            className="input input-bordered w-full rounder-r-none resize-none
+            className="input input-bordered w-full rounded-r-none resize-none
             overflow-y-auto max-h-40"
         ></textarea>
     );
