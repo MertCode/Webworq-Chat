d0f3e035e3490f25f1d2dff7be7ef67e344b15f7
MertCode <mertcode0@gmail.com>
2024-06-14 02:54:04 +0200

    input field added in the chat messages included paperclip to add files, images emoji and thumb's up

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,9 +1,10 @@
-import ConversationHeader from "@/Components/App/ConversationHeader";
-import MessageItem from "@/Components/App/MessageItem";
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import ChatLayout from "@/Layouts/ChatLayout";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/solid";
 import { useEffect, useRef, useState } from "react";
+import ConversationHeader from "@/Components/App/ConversationHeader";
+import MessageItem from "@/Components/App/MessageItem";
+import MessageInput from "@/Components/App/MessageInput";
 
 function Home({ selectedConversation = null, messages = null }) {
     const [localMessages, setLocalMessages] = useState([]);
@@ -62,7 +63,7 @@ function Home({ selectedConversation = null, messages = null }) {
                         )}
                     </div>
                     {/* will stay frozen */}
-                    {/* <MessageInput conversation={selectedConversation} /> */}
+                    <MessageInput conversation={selectedConversation} />
                 </>
             )}
         </>
