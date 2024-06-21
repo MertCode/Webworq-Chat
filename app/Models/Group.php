4347b87349463ffd3f28c0c02b2fd745d2eea323
MertCode <mertcode0@gmail.com>
2024-06-21 02:55:47 +0200

    Create Groups and add users - Edit or delete groups. This will start a background job, so that if the group is large and needs several minutes to be deleted,   it will be deleted in background and will notify users using websockets.

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -9,7 +9,12 @@ class Group extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'description', 'owner_id', 'last_message_id'];
+    protected $fillable = [
+        'name',
+        'description',
+        'owner_id',
+        'last_message_id',
+    ];
 
     public function users()
     {
@@ -26,10 +31,15 @@ class Group extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function lastMessage()
+    {
+        return $this->belongsTo(Message::class, 'last_message_id');
+    }
+
     public static function getGroupsForUser(User $user)
     {
         $query = self::select(['groups.*', 'messages.message as last_message', 'messages.created_at as last_message_date'])
-            ->join('group_users', 'groups.id', '=', 'group_users.group_id')
+            ->join('group_users', 'group_users.group_id', '=', 'groups.id')
             ->leftJoin('messages', 'messages.id', '=', 'groups.last_message_id')
             ->where('group_users.user_id', $user->id)
             ->orderBy('messages.created_at', 'desc')
@@ -46,9 +56,9 @@ class Group extends Model
             'description' => $this->description,
             'is_group' => true,
             'is_user' => false,
-            'owner' => $this->owner_id,
+            'owner_id' => $this->owner_id,
             'users' => $this->users,
-            'users_ids' => $this->users->pluck('id'),
+            'user_ids' => $this->users->pluck('id'),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
             'last_message' => $this->last_message,
@@ -58,9 +68,10 @@ class Group extends Model
 
     public static function updateGroupWithMessage($groupId, $message)
     {
+        // Create or update group with received group id and message
         return self::updateOrCreate(
-            ['id' => $groupId],
-            ['last_message_id' => $message->id]
+            ['id' => $groupId], // search conditions
+            ['last_message_id' => $message->id] // values to update
         );
     }
 }
