3648e54ff45afc48698055abe404f2dbd0fcf1d1
MertCode <mertcode0@gmail.com>
2024-06-23 16:51:13 +0200

    imported fir]a code font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,8 +10,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500;700&display=swap" rel="stylesheet">
-
+    <link href='https://fonts.googleapis.com/css?family=Fira Code' rel='stylesheet'>
 
     <!-- Scripts -->
     @routes
