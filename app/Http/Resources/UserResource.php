5ffb80f54f8c2850bdbbc0011a21726ce6d21a5f
MertCode <mertcode0@gmail.com>
2024-06-23 17:02:49 +0200

    Add UserResource with avatar URL and last message details

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Support\Facades\Storage;
 
 class UserResource extends JsonResource
 {
-
     public static $wrap = false;
+
     /**
      * Transform the resource into an array.
      *
@@ -19,12 +19,12 @@ class UserResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'avatar' => $this->avatar ? Storage::url($this->avatar) : null,
+            'avatar_url' => $this->avatar ? Storage::url($this->avatar) : null,
             'name' => $this->name,
             'email' => $this->email,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
-            'is_Admin' => (bool) $this->is_Admin,
+            'is_admin' => (bool) $this->is_admin,
             'last_message' => $this->last_message,
             'last_message_date' => $this->last_message_date,
         ];
